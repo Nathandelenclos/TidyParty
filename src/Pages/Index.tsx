513bcfc152f0a40455280855ef6ff127f6bdbb61
Nather_ / Nathan Delenclos <nathan.delenclos@epitech.eu>
2024-01-24 02:25:19 +0100

    fix: SEO

--- src/Pages/Index.tsx
+++ src/Pages/Index.tsx
@@ -17,18 +17,6 @@ export const Index: React.FC = () => {
     <div>
       <Helmet>
         <title>Accueil - TidyParty</title>
-        <meta name="keywords" content="tidyParty, party, tidy" />
-        <meta name="author" content="Nathan DELENCLOS" />
-        <meta property="og:title" content="Accueil - TidyParty" />
-        <meta
-          property="og:description"
-          content="Elevate every gathering into a memorable celebration with Tidy Party: the ultimate app for effortlessly organizing unforgettable soirées with style and ease!"
-        />
-        <meta
-          property="og:image"
-          content="https://www.francebleu.fr/s3/cruiser-production/2023/04/ea0c723d-d743-4204-9c50-4153e6769d8e/1200x680_sc_gettyimages-451813251.jpg"
-        />
-        <meta property="og:url" content="https://nathandelenclos.github.io/TidyParty/" />
       </Helmet>
       <Container style={{
         display: 'flex',
